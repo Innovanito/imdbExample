4e374417de5371e5aac32d228420f145f216dcc8
Innovanito <khg05311@naver.com>
2021-08-29 23:27:32 +0900

    Fifth version updated

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <HeadLine />
+  <Search />
 </template>
 
 <script>
 import HeadLine from '~/components/HeadLine.vue'
+import Search from '~/components/Search.vue'
+
 
 export default {
   components: {
-    HeadLine
+    HeadLine,
+    Search
   }
 }
 </script>
