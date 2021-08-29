63a31b939abdb17913f1396dc4cfba34b9e0e208
Innovanito <khg05311@naver.com>
2021-08-29 19:51:56 +0900

    Fourth version updated

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,6 +1,13 @@
 <template>
-  <h1>Homme</h1>
-  <div class="btn btn-primary">
-    Home
-  </div>
+  <HeadLine />
 </template>
+
+<script>
+import HeadLine from '~/components/HeadLine.vue'
+
+export default {
+  components: {
+    HeadLine
+  }
+}
+</script>
